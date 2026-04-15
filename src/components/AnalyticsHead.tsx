ac9095c6d0ae47090957fd4dd7dcbd22904c3553
GenGo2 <reg@gengo.online>
2026-04-15 12:01:39 +0300

    Update AnalyticsHead.tsx

--- src/components/AnalyticsHead.tsx
+++ src/components/AnalyticsHead.tsx
@@ -36,7 +36,9 @@ export function AnalyticsHead({ googleAnalyticsId, yandexMetrikaId, customHeadSc
           />
           <Script id="google-analytics" strategy="afterInteractive">
             {`
-              window.dataLayer = window.dataLayer || [];
+              if (typeof window !== 'undefined') {
+  window.dataLayer = window.dataLayer || [];
+}
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${googleAnalyticsId}');
@@ -45,17 +47,19 @@ export function AnalyticsHead({ googleAnalyticsId, yandexMetrikaId, customHeadSc
         </>
       )}
       {yandexMetrikaId && (
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(${yandexMetrikaId}, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
-          `}
-        </Script>
-      )}
+  <Script id="yandex-metrika" strategy="afterInteractive">
+    {`
+      if (typeof window !== 'undefined') {
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(${yandexMetrikaId}, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
+      }
+    `}
+  </Script>
+)}
     </>
   )
 }
