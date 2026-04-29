2dbc70cda221dc614c0ddb4e23f0ccbefddacde8
GenGo2 <reg@gengo.online>
2026-04-29 14:52:10 +0300

    Update layout.tsx

--- src/app/(standalone)/layout.tsx
+++ src/app/(standalone)/layout.tsx
@@ -1,3 +1,21 @@
+import type { Metadata, Viewport } from 'next';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+};
+
+export const metadata: Metadata = {
+  // Мета-теги для корректного отображения в WebView
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+  },
+};
+
 export default function StandaloneLayout({
   children,
 }: {
