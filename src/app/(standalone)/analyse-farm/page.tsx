49789b2e91503886edf1406fe956b449181a7f4c
GenGo2 <reg@gengo.online>
2026-05-20 13:45:32 +0300

    Create page.tsx

---
+++ src/app/(standalone)/analyse-farm/page.tsx
@@ -0,0 +1,12 @@
+import { StickyCta } from './_components/StickyCta';
+import { ClientMetricsWrapper } from './_components/ClientMetricsWrapper';
+
+export const dynamic = 'force-dynamic';
+
+export const metadata = {
+  title: 'Контент-ферма АНАЛИЗ',
+  description: 'Узнайте, сколько просмотров теряет ваш контент прямо сейчас.',
+};
+
+
+import FarmPageClient from './FarmPageClient';
